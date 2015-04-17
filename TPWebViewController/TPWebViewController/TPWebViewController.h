a24f4f3c4ba41a76f5d11d833e501bd2dd33a0a9
Timm Preetz <timm@preetz.us>
2015-04-17 16:16:26 +0200

    v 2.1.0

---
+++ TPWebViewController/TPWebViewController/TPWebViewController.h
@@ -0,0 +1,19 @@
+//
+//  TPWebViewController.h
+//  TPWebViewController
+//
+//  Created by Timm Preetz on 9.4.2015.
+//  Copyright (c) 2015 Timm Preetz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPWebViewController.
+FOUNDATION_EXPORT double TPWebViewControllerVersionNumber;
+
+//! Project version string for TPWebViewController.
+FOUNDATION_EXPORT const unsigned char TPWebViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPWebViewController/PublicHeader.h>
+
+
---
+++ TPWebViewController/TPWebViewControllerTests/TPWebViewControllerTests.m
@@ -0,0 +1,40 @@
+//
+//  TPWebViewControllerTests.m
+//  TPWebViewControllerTests
+//
+//  Created by Timm Preetz on 9.4.2015.
+//  Copyright (c) 2015 Timm Preetz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TPWebViewControllerTests : XCTestCase
+
+@end
+
+@implementation TPWebViewControllerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
